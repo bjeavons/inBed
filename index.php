35da26799df99e5afb465f185621f81ab044bd99
Ben Jeavons <benjamin.jeavons@colorado.edu>
2011-07-16 00:26:57 -0400

    Functionify and filling out htaccess

--- index.php
+++ index.php
@@ -1,25 +1,43 @@
 <?php
 
-require 'QueryPath/QueryPath.php';
-
-$uri = $_GET['q'];
-$page = htmlqp($uri);
-
-//$page->branch('h2')->append(' in bed');
-
-foreach (qp($page, 'h2 a') as $header) {
-  $header->replaceWith($header->innerHTML() . 'in bed');
-  $header->append(' in bed');
+function validURL($scheme, $url) {
+  return true; // @todo
 }
 
-foreach (qp($page, 'h3 a') as $header) {
-  //$header->replaceWith($header->innerHTML() . 'in bed');
-  $header->append(' in bed');
+function inBedify($uri) {
+  require 'QueryPath/QueryPath.php';
+  $page = htmlqp($uri);
+
+  //$page->branch('h2')->append(' in bed');
+
+  foreach (qp($page, 'h2 a') as $header) {
+    $header->replaceWith($header->innerHTML() . 'in bed');
+    $header->append(' in bed');
+  }
+
+  foreach (qp($page, 'h3 a') as $header) {
+    //$header->replaceWith($header->innerHTML() . 'in bed');
+    $header->append(' in bed');
+  }
+
+  print 'bar';//$page->html();
+  exit;
 }
 
-/*$heading = $bodyqp->branch('h1')->remove()->text();
-      $body = $bodyqp->innerHTML();*/
-print_r($page->html());
+function frontPage() {
+  print 'foo';
+  exit;
+}
 
+if (isset($_GET['q']) && strpos($_GET['q'], 'http') !== false) {
+  // A forward slash was stripped.
+  list($scheme, $url) = explode(':/', $_GET['q']);
+  // Validate URL.
+  if (validURL($scheme, $url)) {
+    inBedify($url);
+  }
+}
+
+frontPage();
 
 ?>
