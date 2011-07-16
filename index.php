46c7230a5a5cd7bf0702d7658e3cf7aea2993c57
Ben Jeavons <benjamin.jeavons@colorado.edu>
2011-07-16 00:32:41 -0400

    comments and getting h1

--- index.php
+++ index.php
@@ -8,36 +8,38 @@ function inBedify($uri) {
   require 'QueryPath/QueryPath.php';
   $page = htmlqp($uri);
 
-  //$page->branch('h2')->append(' in bed');
-
-  foreach (qp($page, 'h2 a') as $header) {
-    $header->replaceWith($header->innerHTML() . 'in bed');
+  foreach (qp($page, 'h1 a') as $header) {
     $header->append(' in bed');
   }
-
+  foreach (qp($page, 'h2 a') as $header) {
+    $header->append(' in bed');
+  }
   foreach (qp($page, 'h3 a') as $header) {
     //$header->replaceWith($header->innerHTML() . 'in bed');
     $header->append(' in bed');
   }
 
-  print 'bar';//$page->html();
+  print $page->html();
   exit;
 }
 
 function frontPage() {
-  print 'foo';
+  print <<<eof
+  
+>>>eof;
   exit;
 }
+
 
 if (isset($_GET['q']) && strpos($_GET['q'], 'http') !== false) {
   // A forward slash was stripped.
   list($scheme, $url) = explode(':/', $_GET['q']);
   // Validate URL.
   if (validURL($scheme, $url)) {
-    inBedify($url);
+    $url = $scheme . '://' . $url;
+    inBedify($url); // inBedify() exists;
   }
 }
 
-frontPage();
-
+frontPage(); // frontPage exists;
 ?>
